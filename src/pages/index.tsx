006b58056b081953cabe7ae718c568a7d0771643
Richard <richard@protonmail.com>
2020-09-08 17:45:48 +0200

    Review

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -117,7 +117,7 @@ export default function Home({ data }: Props) {
     }
 
     return (
-        <div>
+        <>
             {checked === 'about' ? (
                 <AboutProton windowSize={windowWidth} />
             ) : (
@@ -162,7 +162,7 @@ export default function Home({ data }: Props) {
                     )
                 })}
             </div>
-        </div>
+        </>
     )
 }
 
